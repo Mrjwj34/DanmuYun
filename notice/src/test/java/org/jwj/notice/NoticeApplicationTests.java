8aa8f20f2ed3d209103e2564a6a4040672745fed
Mrjwj34 <jwj3050667529@gmail.com>
2025-03-27 23:06:44 +0800

    初始化提交

---
+++ notice/src/test/java/org/jwj/notice/NoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jwj.notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
